0b493d9a4e6f30cf450b49963303eb52ad948ced
Pascal Thomet <pthomet@gmail.com>
2021-01-28 20:50:41 +0100

    Add project imgui_demowindow_only.cpp

--- src/imgui_demowindow_only/imgui_demowindow_only.cpp
+++ src/imgui_demowindow_only/imgui_demowindow_only.cpp
@@ -1,4 +1,13 @@
-//
-// Created by Pascal Thomet on 1/28/21.
-//
+#include "hello_imgui/hello_imgui.h"
 
+int main(int , char *[])
+{
+    HelloImGui::RunnerParams params;
+    params.appWindowParams.windowSize = {1280.f, 720.f};
+    params.appWindowParams.windowTitle = "ImGui Demo Window Only";
+    params.imGuiWindowParams.defaultImGuiWindowType = HelloImGui::DefaultImGuiWindowType::NoDefaultWindow;
+
+    params.callbacks.ShowGui = [&]() { ImGui::ShowDemoWindow(); };
+    HelloImGui::Run(params);
+    return 0;
+}
