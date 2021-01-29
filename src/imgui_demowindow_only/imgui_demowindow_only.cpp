91d0e01aa687b5a665d764e4caa84360e28f2d17
Pascal Thomet <pthomet@gmail.com>
2021-01-29 10:54:26 +0100

    update imgui submodule

--- src/imgui_demowindow_only/imgui_demowindow_only.cpp
+++ src/imgui_demowindow_only/imgui_demowindow_only.cpp
@@ -3,7 +3,7 @@
 int main(int , char *[])
 {
     HelloImGui::RunnerParams params;
-    params.appWindowParams.windowSize = {1280.f, 720.f};
+    params.appWindowParams.windowSize = {1280.f, 800.f};
     params.appWindowParams.windowTitle = "ImGui Demo Window Only";
     params.imGuiWindowParams.defaultImGuiWindowType = HelloImGui::DefaultImGuiWindowType::NoDefaultWindow;
 
