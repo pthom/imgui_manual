2310895fbc0c754d800a961440ca7f2553668266
Pascal Thomet <pthomet@gmail.com>
2023-02-16 17:33:55 +0100

    add deploy_imgui_tree.sh

---
+++ tools/deploy_imgui_tree.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+REPO_DIR=$THIS_DIR/..
+cd $REPO_DIR
+
+./tools/emscripten_build.sh
+cd build_emscripten
+rsync -vaz src pascal@traineq.org:HTML/imgui_manual_tree
