8ba5a127abfa496e9592fef5d39ce97d5ae8ef0d
Pascal Thomet <pthomet@gmail.com>
2026-02-04 13:22:02 +0100

    remove oldies

--- src/imgui_example_run/imgui_example_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-THIS_SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-IMGUI_DIR=$THIS_SCRIPT_DIR/../../external/imgui
-cd $IMGUI_DIR/examples/example_sdl_opengl3
-make && ./example_sdl_opengl3
--- src/imgui_example_run/imgui_example_run_emscripten.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-THIS_SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-IMGUI_DIR=$THIS_SCRIPT_DIR/../../external/imgui
-
-source ~/Utils/emsdk/emsdk_env.sh
-cd $IMGUI_DIR/examples/example_emscripten_opengl3
-USE_FILE_SYSTEM=1 make && make serve
